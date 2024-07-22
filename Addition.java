a41e9f1a90a32e1bcd0e35ac243e6c9af5779e83
Abhishek Maurya <maurya972137@gmail.com>
2024-07-22 21:18:01 +0530

    This is my Java Code Challenge

---
+++ Addition.java
@@ -0,0 +1,8 @@
+public class Addition {
+    public static void main(String[] args) {
+        int num_1 = 5, num_2 = 7;
+        int sum;
+        sum = num_1 + num_2;
+        System.out.println("The sum of two number is: " +sum);
+    }
+}
