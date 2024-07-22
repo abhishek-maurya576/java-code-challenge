f34fe7e96138e140a02180497216e82041348c15
Abhishek Maurya <maurya972137@gmail.com>
2024-07-22 21:20:55 +0530

    This is my Java Code Challenge

---
+++ Subtraction.java
@@ -0,0 +1,7 @@
+public class Subtraction {
+    public static void main(String[] args) {
+        int num_1 = 20, num_2 = 10;
+        int sub = num_1 - num_2;
+        System.out.println("The sum of two number is: " +sub);
+    }
+}
