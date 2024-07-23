c678c1c770ae1b8bd56884ac0d7c0b9341afd563
Abhishek Maurya <maurya972137@gmail.com>
2024-07-23 19:52:42 +0530

    This is my Java Code Challenge

---
+++ Division.java
@@ -0,0 +1,8 @@
+public class Division {
+    public static void main(String[] args) {
+        int num_1 = 20, num_2 = 5;
+        int div = num_1 / num_2;
+        System.out.println("The division of two number is: " +div);
+    }
+
+}
---
+++ Multiplication.java
@@ -0,0 +1,7 @@
+public class Multiplication {
+    public static void main(String[] args) {
+        int num_1 = 5, num_2 = 7;
+        int mul = num_1 * num_2;
+        System.out.println("The multiplication of two number is: " +mul);
+    }
+}
